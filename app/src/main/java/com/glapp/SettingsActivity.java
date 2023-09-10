f5834813e24685b5565183e694dc83c67387a947
Marek Gergel <mage26gg@gmail.com>
2023-09-11 01:21:22 +0200

    Preferences code cleanup, Offline mode moved to debug mode

--- app/src/main/java/com/glapp/SettingsActivity.java
+++ app/src/main/java/com/glapp/SettingsActivity.java
@@ -14,7 +14,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentManager;
 import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.MultiSelectListPreference;
@@ -35,10 +34,7 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
             BluetoothService.BluetoothBinder binder = (BluetoothService.BluetoothBinder) service;
             mBluetoothService = binder.getService();
 
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            SettingsFragment settingsFragment = (SettingsFragment) fragmentManager.findFragmentById(R.id.settings);
-            assert settingsFragment != null;
-            settingsFragment.refreshSelectableDevices();
+            updateSelectableDevices();
         }
 
         @Override
@@ -71,10 +67,7 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
         bindService(new Intent(this, BluetoothService.class), mServiceConnection, BIND_AUTO_CREATE);
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        SettingsFragment settingsFragment = (SettingsFragment) fragmentManager.findFragmentById(R.id.settings);
-        assert settingsFragment != null;
-        settingsFragment.debugMode();
+        updateView();
     }
 
     @Override
@@ -96,53 +89,61 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key != null) {
-            if (key.equals("board_data")) {
-                Preference multiSelectListPreference = getPreference(key);
-                if (multiSelectListPreference != null) {
-                    multiSelectListPreference.setSummary(sharedPreferences.getStringSet(key, null).toString());
-                }
+        if (key == null) return;
+
+        switch (key) {
+            case "board_data":
+                getFragmentPreference(key).setSummary(sharedPreferences.getStringSet(key, null).toString());
 
                 // Update board data displayed in offline mode
                 if (sharedPreferences.getBoolean("offline_mode", false)) {
                     mBluetoothService.connect(true);
                 }
+
+                break;
+
+            case "debug_mode":
+                updateView();
+                break;
+
+            case "offline_mode":
+                mBluetoothService.disconnect();
+                break;
+        }
+    }
+
+    private void updateView() {
+        boolean inDebugMode = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("debug_mode", false);
+
+        getFragmentPreference("offline_mode").setVisible(inDebugMode);
+        getFragmentPreference("bluetooth_board_ssid_filter").setVisible(inDebugMode);
+
+        if (!inDebugMode && PreferenceManager.getDefaultSharedPreferences(this).getBoolean("offline_mode", false)) {
+            ((SwitchPreference)getFragmentPreference("offline_mode")).setChecked(false);
+        }
+    }
+
+    private void updateSelectableDevices() {
+        Set<BluetoothDevice> pairedDevices = mBluetoothService.getPairedDevices();
+        String editText = ((EditTextPreference) Objects.requireNonNull(getFragmentPreference("bluetooth_board_ssid_filter"))).getText();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
+            ((ListPreference) Objects.requireNonNull(getFragmentPreference("bluetooth_board_select"))).setEntries(pairedDevices.stream().filter(device -> editText == null || device.getName().contains(editText)).map(device -> device.getName() + "\n" + device.getAddress()).toArray(CharSequence[]::new));
+            ((ListPreference) Objects.requireNonNull(getFragmentPreference("bluetooth_board_select"))).setEntryValues(pairedDevices.stream().filter(device -> editText == null || device.getName().contains(editText)).map(device -> device.getName() + "\n" + device.getAddress()).toArray(CharSequence[]::new));
+        }
+
+        if (((ListPreference) Objects.requireNonNull(getFragmentPreference("bluetooth_board_select"))).getEntry() == null) {
+            if (((ListPreference) Objects.requireNonNull(getFragmentPreference("bluetooth_board_select"))).getEntries().length != 0) {
+                ((ListPreference) Objects.requireNonNull(getFragmentPreference("bluetooth_board_select"))).setValueIndex(0);
             }
         }
     }
 
-    private Preference getPreference(String key) {
+    private Preference getFragmentPreference(String key) {
         SettingsFragment settingsFragment = ((SettingsFragment) getSupportFragmentManager().findFragmentById(R.id.settings));
         return settingsFragment != null ? settingsFragment.findPreference(key) : null;
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
-
-        private void refreshSelectableDevices() {
-            if (getContext() != null && getContext() instanceof SettingsActivity) {
-                Set<BluetoothDevice> pairedDevices = ((SettingsActivity) requireContext()).mBluetoothService.getPairedDevices();
-                String editText = ((EditTextPreference) Objects.requireNonNull(getPreferenceManager().findPreference("bluetooth_board_ssid_filter"))).getText();
-                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_GRANTED) {
-                    ((ListPreference) Objects.requireNonNull(getPreferenceManager().findPreference("bluetooth_board_select"))).setEntries(pairedDevices.stream().filter(device -> editText == null || device.getName().contains(editText)).map(device -> device.getName() + "\n" + device.getAddress()).toArray(CharSequence[]::new));
-                    ((ListPreference) Objects.requireNonNull(getPreferenceManager().findPreference("bluetooth_board_select"))).setEntryValues(pairedDevices.stream().filter(device -> editText == null || device.getName().contains(editText)).map(device -> device.getName() + "\n" + device.getAddress()).toArray(CharSequence[]::new));
-                }
-            }
-
-            if (((ListPreference) Objects.requireNonNull(getPreferenceManager().findPreference("bluetooth_board_select"))).getEntry() == null) {
-                if (((ListPreference) Objects.requireNonNull(getPreferenceManager().findPreference("bluetooth_board_select"))).getEntries().length != 0) {
-                    ((ListPreference) Objects.requireNonNull(getPreferenceManager().findPreference("bluetooth_board_select"))).setValueIndex(0);
-                }
-            }
-        }
-
-        private void debugMode() {
-            if (((SwitchPreference) Objects.requireNonNull(getPreferenceManager().findPreference("debug_mode"))).isChecked()) {
-                ((EditTextPreference) Objects.requireNonNull(getPreferenceManager().findPreference("bluetooth_board_ssid_filter"))).setVisible(true);
-            } else {
-                ((EditTextPreference) Objects.requireNonNull(getPreferenceManager().findPreference("bluetooth_board_ssid_filter"))).setVisible(false);
-            }
-        }
-
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
@@ -165,7 +166,7 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
                                     .setIcon(android.R.drawable.ic_dialog_alert)
                                     .setPositiveButton(android.R.string.ok, (dialog, whichButton) -> {
                                         if (preferences.getBoolean("offline_mode", false)) {
-                                            ((SettingsActivity) requireContext()).mBluetoothService.disconnect();
+                                            ((SettingsActivity) requireActivity()).mBluetoothService.disconnect();
                                         }
                                         preferences.edit().clear().apply();
                                         requireActivity().recreate();
@@ -177,17 +178,9 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
                         }
                     }
                     break;
-                case "debug_mode":
-                    // Debug mode
-                    debugMode();
-                    break;
                 case "bluetooth_board_select":
                     // Refresh paired devices
-                    refreshSelectableDevices();
-                    break;
-                case "offline_mode":
-                    // Offline mode reset
-                    ((SettingsActivity) requireContext()).mBluetoothService.disconnect();
+                    ((SettingsActivity) requireActivity()).updateSelectableDevices();
                     break;
             }
 
